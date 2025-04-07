d2ef7e6e05bc5fbd426a554be1808577f816937f
ArunK <arun.k.dontula@gmail.com>
2025-04-07 12:59:54 +0100

    fixed the packaging

--- src/main/java/com/playwright/demo/DemoApplication.java
+++ src/main/java/com/playwright/demo/DemoApplication.java
@@ -1,7 +1,15 @@
 package com.playwright.demo;
 
+import io.modelcontextprotocol.client.McpSyncClient;
+import io.modelcontextprotocol.spec.McpSchema;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,5 +17,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
